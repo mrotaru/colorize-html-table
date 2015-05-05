2c363264c5a728943690cc3b16862acb7f5caa9e
Mihai Rotaru <mihai.rotaru@gmx.com>
2015-05-05 23:58:29 +0100

    doc, settings object

--- index.js
+++ index.js
@@ -22,15 +22,38 @@
     return 'rgb(' + Math.round(R) + ', ' + Math.round(G) + ', 0)';
   }
 
-  $.fn.colorizeTable = function (table, column) {
-    var column = column || 1;
-    console.log(table);
-    table.find('tr td:nth-child(' + column + ')').each(function() {
-      var n = parseInt($(this).text());
-      if(!isNaN(n)) {
-        console.log(getColorForPercent(n));
-        $(this).css('background-color', getColorForPercent(n));
+  /*
+   * Colorize columns in a table
+   * 
+   * @param {JQuery} table
+   * @param {Integer|Array} column which column(s) should be colorized
+   */
+  $.fn.colorizeTable = function (options) {
+
+    var settings = $.extend({
+      table: null,
+      columns: [1]
+    }, options);
+
+    // colorize a single column
+    function colorizeColumn(columnIndex) {
+      settings.table.find('tr td:nth-child(' + columnIndex + ')').each(function() {
+        var n = parseInt($(this).text());
+        console.log(n);
+        if(!isNaN(n)) {
+          console.log(getColorForPercent(n));
+          $(this).css('background-color', getColorForPercent(n));
+        }
+      });
+    }
+
+    console.log(typeof settings.columns);
+    if(typeof settings.columns == 'number' && settings.columns % 1 === 0) {
+      colorizeColumn(settings.coumns);
+    } else if($.isArray(settings.columns)) {
+      for (var i=0; i < settings.columns.length; ++i) {
+        colorizeColumn(settings.columns[i]);
       }
-    });
+    }
   };
 }));
