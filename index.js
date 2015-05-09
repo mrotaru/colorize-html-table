ea2e1f726747df0ccc5e15257b60d0634b90a15d
Mihai Rotaru <mihai.rotaru@gmx.com>
2015-05-09 22:22:52 +0100

    use hsl

--- index.js
+++ index.js
@@ -8,6 +8,76 @@
   }
 }(function ($) {
 
+  /**
+   * Converts an RGB color value to HSL. Conversion formula
+   * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+   * Assumes r, g, and b are contained in the set [0, 255] and
+   * returns h, s, and l in the set [0, 1].
+   * from: http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+   *
+   * @param   Number  r       The red color value
+   * @param   Number  g       The green color value
+   * @param   Number  b       The blue color value
+   * @return  Array           The HSL representation
+   */
+  function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+      h = s = 0; // achromatic
+    }else{
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max){
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+    }
+
+    return {h: h, s: s, l: l};
+  }
+
+  /**
+   * Converts an HSL color value to RGB. Conversion formula
+   * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+   * Assumes h, s, and l are contained in the set [0, 1] and
+   * returns r, g, and b in the set [0, 255].
+   * from: http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+   *
+   * @param   Number  h       The hue
+   * @param   Number  s       The saturation
+   * @param   Number  l       The lightness
+   * @return  Array           The RGB representation
+   */
+  function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+      r = g = b = l; // achromatic
+    }else{
+      function hue2rgb(p, q, t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+      }
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1/3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return {r: Math.round(r*255), g: Math.round(g*255), b: Math.round(b*255)};
+  }
+  
   /*
    * Colorize columns in a table
    * 
@@ -44,10 +114,9 @@
       if(settings.invert) {
         percent = 100 - percent;
       }
-      R = (255 * percent) / 100;
-      G = (255 * (100 - percent)) / 100;
-      B = 0;
-      return 'rgb(' + Math.round(R) + ', ' + Math.round(G) + ', 0)';
+      var hue = (percent*33)/100/100;
+      var color = hslToRgb(hue, 1, 0.35);
+      return 'rgb(' + Math.round(color.r) + ', ' + Math.round(color.g) + ', ' + Math.round(color.b) + ')';
     }
 
     /**
@@ -76,16 +145,14 @@
 
       // used for calculating percentage
       var difference = max - min;
-      console.log(max,min);
-      console.log('diff: ', difference);
 
       // go over each cell and colorize it
       settings.table.find('tr td:nth-child(' + columnIndex + ')').each(function() {
         var n = parseFloat($(this).text());
         if(!isNaN(n)) {
           var percentage = (n - min) * 100 / difference;
-          console.log('perc: ', percentage);
-          $(this).css('background-color', getColorForPercent(percentage));
+          var color = getColorForPercent(percentage);
+          $(this).css('background-color', color);
         }
       });
     }
